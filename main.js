77425e87a96b276507151b8fbe43bf7fbdc3664b
mr0ak1 <abhishekkrtiwary3395@gmail.com>
2025-08-01 10:16:52 +0530

    connecting db

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ import express from "express"
 import mongoose from "mongoose"
 import User from "./model/user.js"
 const app = express()
+
+mongoose
+  .connect("mongodb://localhost:27017/myapp")
+  .then((data) => console.log("connected to db"))
+  .catch((err) => console.log(err))
